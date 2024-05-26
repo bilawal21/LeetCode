d8c460a006aa4081909afce51d4f9f5a44e272a2
bilawal21 <95687983+bilawal21@users.noreply.github.com>
2024-05-26 21:58:26 +0500

    Time: 61 ms (57.08%), Space: 17.8 MB (16.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,28 +1,17 @@
-from typing import List
-
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        complement_dict = {}
+    def twoSum(self, nums, target):
+        prevMap = {} # val : index
+        
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
+        
 
-        # Iterate through the array
-        for i, num in enumerate(nums):
-            # Calculate the complement needed to reach the target
-            complement = target - num
-
-            # Check if the complement is already in the dictionary
-            if complement in complement_dict:
-                # If found, return the indices of the two numbers
-                return [complement_dict[complement], i]
-
-            # If not found, store the current number and its index in the dictionary
-            complement_dict[num] = i
-
-        # If no solution is found
-        return None
-
-# Example usage:
-solution = Solution()
-nums = [2, 7, 11, 15]
+obj = Solution()
+input_nums = [2,7,11,15]
 target = 9
-result = solution.twoSum(nums, target)
+result = obj.twoSum(input_nums, target)
 print(result)
