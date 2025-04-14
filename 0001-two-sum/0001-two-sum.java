9e9d0b5680f354d68caef4443e4647398175bc85
Bilawal <95687983+bilawal21@users.noreply.github.com>
2025-04-14 12:44:21 +0500

    Time: 2 ms (98.83%), Space: 45.4 MB (10.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,19 +3,20 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i]; 
+            int diff = target - nums[i];
 
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+            if (map.containsKey(diff)){
+                return new int[] {map.get(diff), i};
             }
 
             map.put(nums[i], i);
         }
 
-        return new int[] {};
+        return new int[]{-1,-1};
         
         
-        
+        // Time Complexity = O(n)
+        // Space Complexity = O(n)
         
         
         
